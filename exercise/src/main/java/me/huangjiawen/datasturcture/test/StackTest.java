16c57313e35e401df83b707d341896e59ff316e6
huangjiawendanny@gmail.com <hjw541988478>
2017-03-09 22:17:06 +0800

    重新导入AS为JavaPlugin工程

---
+++ exercise/src/main/java/me/huangjiawen/datasturcture/test/StackTest.java
@@ -0,0 +1,44 @@
+package me.huangjiawen.datasturcture.test;
+
+import me.huangjiawen.datasturcture.statck.ElemArithExpressionEvaluation;
+import me.huangjiawen.datasturcture.statck.Stack;
+import me.huangjiawen.datasturcture.statck.linked.LinkedStack;
+import me.huangjiawen.datasturcture.statck.sequential.SequentialStack;
+
+public class StackTest {
+
+    public static void main(String[] args) {
+        Stack<Integer> seqStack = new SequentialStack<Integer>();
+        seqStack.push(1);
+        seqStack.push(2);
+        seqStack.push(3);
+        seqStack.push(4);
+        seqStack.push(5);
+        seqStack.print();
+        seqStack.pop();
+        seqStack.push(8);
+        seqStack.push(9);
+        seqStack.print();
+        System.out.println("--------------------------------");
+        Stack<Integer> linkedStack = new LinkedStack<Integer>();
+        linkedStack.push(1);
+        linkedStack.push(2);
+        linkedStack.push(3);
+        linkedStack.push(4);
+        linkedStack.push(5);
+        linkedStack.print();
+        linkedStack.pop();
+        linkedStack.push(8);
+        linkedStack.push(9);
+        linkedStack.print();
+        System.out.println("---------------------------------");
+        ElemArithExpressionEvaluation expEva = new ElemArithExpressionEvaluation();
+        System.out.println(expEva.evaluate(
+                expEva.convertToRPN(
+                        expEva.sliceStringToArray("20+(3-1)*22+10/2"))));
+
+        System.out.println(expEva.calculate("3-2+2"));
+        System.out.println(expEva.calculate("(1+(4+5+2)-3)+(6+8)"));
+    }
+
+}
