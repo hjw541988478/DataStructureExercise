1d48525e6595348b3c112ee756982fd329f07937
garvin <huangjiawendanny@gmail.com>
2018-08-01 22:21:54 +0800

    5. 5. Longest Palindromic Substring 解题 ，思路： 采用DP，遍历出所有的情况，基础案例分奇偶，记录最长的起始值和长度，便可以拿到最长回文子串

---
+++ exercise/src/main/java/ds/exercise/leetcode/LongestPalindrome.java
@@ -0,0 +1,35 @@
+package ds.exercise.leetcode;
+
+public class LongestPalindrome {
+    private int maxlen, start;
+
+    private void judgePalindrome(String s, int j, int k) {
+        while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+            j--;
+            k++;
+        }
+        if (maxlen < k - j - 1) {
+            start = j + 1;
+            maxlen = k - j - 1;
+        }
+    }
+
+    /**
+     * 5. Longest Palindromic Substring
+     */
+    public String longestPalindrome(String s) {
+        if (s.length() == 1) {
+            return s;
+        }
+        for (int i = 0; i < s.length() - 1; i++) {
+            judgePalindrome(s, i, i);
+            judgePalindrome(s, i, i + 1);
+        }
+        return s.substring(start, start + maxlen);
+    }
+
+    public static void main(String[] args) {
+        LongestPalindrome l = new LongestPalindrome();
+        System.out.println(l.longestPalindrome("abcba"));
+    }
+}
