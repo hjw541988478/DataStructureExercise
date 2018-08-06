b280e8bb284410e4c27f37d283a5dac4cff145b0
garvin <huangjiawendanny@gmail.com>
2018-08-06 21:16:34 +0800

    11. Container With Most Water 解题思路： 起点和终点，往中间不断寻找较小的值乘以坐标差值，不断遍历寻找到最大值

---
+++ exercise/src/main/java/ds/exercise/leetcode/MostWaterContainer.java
@@ -0,0 +1,32 @@
+package ds.exercise.leetcode;
+
+/**
+ * 11. Container With Most Water
+ */
+public class MostWaterContainer {
+    public int maxArea(int[] height) {
+        if (height == null || height.length == 0 || height.length == 1) {
+            return 0;
+        }
+        int maxArea = 0;
+        int left = 0, right = height.length - 1;
+        while (left < right) {
+            boolean leftSmaller = height[left] < height[right];
+            int area = (leftSmaller ? height[left] : height[right]) * (right - left);
+            if (area > maxArea) {
+                maxArea = area;
+            }
+            if (leftSmaller) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        MostWaterContainer mostWaterContainer = new MostWaterContainer();
+        System.out.println(mostWaterContainer.maxArea(new int[]{1, 2, 4, 3}));
+    }
+}
