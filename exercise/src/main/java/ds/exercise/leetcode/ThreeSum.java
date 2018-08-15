70524fcf2763280ff13c968dadfccd1365da3ae9
garvin <huangjiawendanny@gmail.com>
2018-08-16 00:48:50 +0800

    16. 3Sum Closest 解题思路： 先升序排序，从头和尾往中靠拢，如果三数之和大于target，则右边缩进一位，否则左边扩大一位，边遍历的同时检查是否有更合适的数并记录。

--- exercise/src/main/java/ds/exercise/leetcode/ThreeSum.java
+++ exercise/src/main/java/ds/exercise/leetcode/ThreeSum.java
@@ -77,6 +77,29 @@ public class ThreeSum {
         return res;
     }
 
+    /**
+     * 16. 3Sum Closest
+     */
+    public int threeSumClosest(int[] num, int target) {
+        int result = num[0] + num[1] + num[num.length - 1];
+        Arrays.sort(num);
+        for (int i = 0; i < num.length - 2; i++) {
+            int start = i + 1, end = num.length - 1;
+            while (start < end) {
+                int sum = num[i] + num[start] + num[end];
+                if (sum > target) {
+                    end--;
+                } else {
+                    start++;
+                }
+                if (Math.abs(sum - target) < Math.abs(result - target)) {
+                    result = sum;
+                }
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         new ThreeSum().threeSum(new int[]{-1, 0, 1, 2, -1, -4});
     }
