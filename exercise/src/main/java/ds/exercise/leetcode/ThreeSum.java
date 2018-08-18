0622477ca38964290f8143aa33ec8edec25f2b87
Garvin <huangjiawendanny@gmail.com>
2018-08-19 00:14:20 +0800

    18. 4Sum 解题思路： 沿用3sum的思路，将数组升序后，逐个比较，第一层遍历到倒数第4个数，第二层遍历到倒数第3个数，最后两个只要找到求2Sum的方法便可以找到所有的情况，注意此处也可以优化的地方，重复的可以跳过比较。

--- exercise/src/main/java/ds/exercise/leetcode/ThreeSum.java
+++ exercise/src/main/java/ds/exercise/leetcode/ThreeSum.java
@@ -100,7 +100,47 @@ public class ThreeSum {
         return result;
     }
 
+    /**
+     * 18. 4Sum
+     */
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length < 4) {
+            return res;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                int l = j + 1, r = nums.length - 1;
+                while (l < r) {
+                    if (nums[i] + nums[j] + nums[l] + nums[r] == target) {
+                        res.add(Arrays.asList(nums[i], nums[j], nums[l], nums[r]));
+                        l++;
+                        r--;
+                        while (l < r && nums[l] == nums[l - 1]) {
+                            l++;
+                        }
+                        while (l < r && nums[r] == nums[r + 1]) {
+                            r--;
+                        }
+                    } else if (nums[i] + nums[j] + nums[l] + nums[r] < target) {
+                        l++;
+                    } else {
+                        r--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
     public static void main(String[] args) {
         new ThreeSum().threeSum(new int[]{-1, 0, 1, 2, -1, -4});
+        System.out.println(new ThreeSum().fourSum(new int[]{0, 0, 0, 0}, 0));
     }
 }
